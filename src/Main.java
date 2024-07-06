7cfa37d236e0a27de842ba82b515fe420b9d33ae
Hans <ishigoemon878@gmail.com>
2024-07-06 20:09:37 +0700

    balikin main jadi new loginPage()

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import view.LoginPage;
-import view.RegisterVehiclePage;
 
 public class Main {
     public static void main(String[] args) {
-        new RegisterVehiclePage();
+        new LoginPage();
     }
 }
