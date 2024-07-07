af57217aa1181778a677ac563b11a33e71ea9651
StanislausJoel <stanislausjoel.fa@gmail.com>
2024-07-07 18:40:16 +0700

    fix encryptor

--- src/controller/Encryptor.java
+++ src/controller/Encryptor.java
@@ -1,80 +1,23 @@
 package controller;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Encryptor {
 
-    private static final int DEFAULT_COLUMNS = 4;
+    public static String hash(String plaintext) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(plaintext.getBytes());
 
-    public static String encrypt(String plaintext) {
-
-        int index = 0;
-        int rows = (int) Math.ceil((double) plaintext.length() / DEFAULT_COLUMNS);
-        char[][] grid = new char[rows][DEFAULT_COLUMNS];
-
-        for (int r = 0; r < rows; r++) {
-
-            for (int c = 0; c < DEFAULT_COLUMNS; c++) {
-
-                if (index < plaintext.length()) {
-
-                    grid[r][c] = plaintext.charAt(index++);
-
-                }
-                else {
-
-                    grid[r][c] = ' ';
-
-                }
-
+            StringBuilder hashString = new StringBuilder();
+            for (byte b : hashBytes) {
+                hashString.append(String.format("%02x", b));
             }
 
+            return hashString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Hashing algorithm not found", e);
         }
-
-        StringBuilder ciphertext = new StringBuilder();
-
-        for (int c = 0; c < DEFAULT_COLUMNS; c++) {
-
-            for (int r = 0; r < rows; r++) {
-
-                ciphertext.append(grid[r][c]);
-
-            }
-
-        }
-
-        return ciphertext.toString();
-
     }
-
-    public static String decrypt(String ciphertext) {
-
-        int index = 0;
-        int rows = (int) Math.ceil((double) ciphertext.length() / DEFAULT_COLUMNS);
-        char[][] grid = new char[rows][DEFAULT_COLUMNS];
-
-        for (int c = 0; c < DEFAULT_COLUMNS; c++) {
-
-            for (int r = 0; r < rows; r++) {
-
-                grid[r][c] = ciphertext.charAt(index++);
-
-            }
-
-        }
-
-        StringBuilder plaintext = new StringBuilder();
-
-        for (int r = 0; r < rows; r++) {
-
-            for (int c = 0; c < DEFAULT_COLUMNS; c++) {
-
-                plaintext.append(grid[r][c]);
-
-            }
-
-        }
-
-        return plaintext.toString();
-
-    }
-
 }
