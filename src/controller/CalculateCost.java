07976b045d9cbc08688cb3f419abd3414a9e5866
Hannah Debora <hnnhdbr@gmail.com>
2024-07-12 20:45:05 +0700

    done calculate cost

--- src/controller/CalculateCost.java
+++ src/controller/CalculateCost.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.Class.order.OrderItem;
+
 public class CalculateCost {
     public static double calculateGoRide(double distance, double fareKm) {
         return fareKm * distance;
@@ -13,8 +15,13 @@ public class CalculateCost {
         return (fareKm * distance) + (fare5Kg * ((int) weight / 5));
     }
 
-    public static double calculateGoFood(double distance, double fareKm, double[] listHargaItem) {
+    public static double calculateGoFood(double distance, double fareKm, OrderItem[] cart) {
         double totalHarga = 0;
-        return totalHarga;
+
+        for (OrderItem c : cart) {
+            totalHarga += (c.getJumlah() * c.getItem().getHarga());
+        }
+
+        return (fareKm * distance) + totalHarga;
     }
 }
